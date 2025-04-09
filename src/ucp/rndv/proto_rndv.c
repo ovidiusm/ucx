2157ad0ec6f40ffac0ca806e12f01413c6173cff
Ovidiu Mara <ovidiusm@nvidia.com>
2025-04-09 10:15:04 +0300

    cfghack

--- src/ucp/rndv/proto_rndv.c
+++ src/ucp/rndv/proto_rndv.c
@@ -82,9 +82,9 @@ ucp_proto_rndv_ctrl_get_md_map(const ucp_proto_rndv_ctrl_init_params_t *params,
                       context->tl_mds[md_index].rsc.md_name, md_index);
         *md_map |= UCS_BIT(md_index);
         
-        if (strcmp(context->tl_mds[md_index].rsc.md_name, "cuda_ipc") == 0) {
-            //cuda_md_map = UCS_BIT(md_index);
-            //cuda_sys_dev_map = UCS_BIT(ep_sys_dev);
+        if (context->config.ext.max_rndv_lanes == 1 && strcmp(context->tl_mds[md_index].rsc.md_name, "cuda_ipc") == 0) {
+            cuda_md_map = UCS_BIT(md_index);
+            cuda_sys_dev_map = UCS_BIT(ep_sys_dev);
         }
 
         if (ep_sys_dev >= UCP_MAX_SYS_DEVICES) {
